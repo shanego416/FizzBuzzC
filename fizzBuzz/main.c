11ff22ced6e5026a3ecd50a248ae5b3930f724f6
shanego416 <88070204+shanego416@users.noreply.github.com>
2023-02-12 17:17:44 -0500

    Update main.c

--- fizzBuzz/main.c
+++ fizzBuzz/main.c
@@ -4,6 +4,10 @@
 //
 //  Created by shanego416 on 2022-02-21.
 
+/*Create a program that will take a user's input, and print every number before n replacing numbers divisible
+by 3 with the word Fizz, numbers divisible by 5 with the word Buzz and numbers divisible by both 3 and 5 with
+FizzBuzz*/
+
 #include <stdio.h>
 #include <math.h>
 
